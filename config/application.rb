c96d6e401c7a34aecd04959c7e7c6f0c8f5f5707
Shunpei0902 <shunkotkg0141@gmail.com>
2023-03-27 18:35:19 +0900

    sample_deploy_eleventh commit

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module HandMovies
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # デプロイするサービスのホストを追加する
+    config.hosts << 'xxx.onrender.com'
   end
 end
